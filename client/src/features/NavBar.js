4516ffde1b51dd0eb69295a60a51d9a1a252c269
Colin Smith <88501518+colinksmith@users.noreply.github.com>
2023-08-07 20:00:56 -0400

    added logo and famicon

--- client/src/features/NavBar.js
+++ client/src/features/NavBar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/bb-price-logo.png'
 
 import { Fragment, useState } from 'react'
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
@@ -38,7 +39,7 @@ export default function NavBar() {
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                <img className="h-16 w-auto" src={logo} alt="" />
               </a>
             </div>
             <div className="flex lg:hidden">
